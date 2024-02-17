4b167f0e7132d27af59299c99156f68f54aa0763
Harukotokoto <n_rui@outlook.jp>
2024-02-18 02:28:43 +0900

    整形

--- src/index.ts
+++ src/index.ts
@@ -17,15 +17,3 @@ process.on('unhandledRejection', async (e) => {
   client.Logger.error(e);
   return e;
 });
-
-client.on('messageCreate', async (message) => {
-  const members = await message.guild?.members.fetch();
-  members
-    ?.map((member) => member.id)
-    .forEach(async (memberId) => {
-      const member = message.guild?.members.cache.get(memberId);
-      await member?.roles.remove('1200102978108010658');
-      await member?.roles.add('1122864290496528424');
-      await message.reply(`${member?.toString()}を解放しました`);
-    });
-});
